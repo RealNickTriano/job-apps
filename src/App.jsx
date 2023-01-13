c1ba0d05df0da8bc0440e854204b0ede925ab0b3
Nicholas Triano <nicholastriano1999@gmail.com>
2023-01-13 18:38:27 -0500

    input field component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import { useState } from 'react'
+import Input from './components/Input'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmailName] = useState('')
+  const [cellPhone, setCellPhoneName] = useState('')
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className='text-blue-600 text-xl'>Hello World</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-slate-100">
+      <div className='flex flex-col justify-center items-center gap-12'>
+        <Input 
+          name={'First Name'}
+          id={'firstname'}
+          value={firstName}
+          onChange={setFirstName}
+          type={'text'}
+        />
+        <Input 
+          name={'Last Name'}
+          id={'lastname'}
+          value={lastName}
+          onChange={setLastName}
+          type={'text'}
+        />
+        <Input 
+          name={'Email'}
+          id={'email'}
+          value={email}
+          onChange={setEmailName}
+          type={'email'}
+        />
+        <Input 
+          name={'Phone Number'}
+          id={'tel'}
+          value={cellPhone}
+          onChange={setCellPhoneName}
+          type={'tel'}
+        />
+      </div>
     </div>
   )
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Input = ({ id, name, value, onChange, type }) => {
+
+  return (
+    <div className='flex flex-col justify-center items-start font-roboto'>
+        <label 
+            htmlFor={id}
+            className='font-medium text-md'
+        >
+            {name}
+        </label>
+        <input 
+            className='bg-white w-64 h-10 px-2 shadow-md outline-none'
+            type={type} 
+            id={id}
+            name={name}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+        />
+    </div>
+  )
+}
+
+export default Input
